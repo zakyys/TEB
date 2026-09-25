307ad1b7b0be2cded5a34df1bad248b745387f0a
zakyyy16 <mdzakyriady@gmail.com>
2026-09-25 16:29:56 +0700

    chore: refresh dev service worker precache revision

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.haea6286aok"
+    "revision": "0.cs42hg1959"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
